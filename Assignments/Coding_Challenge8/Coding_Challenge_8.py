2afee8df7de81068bbed6f6617984870bc31ab13
Ben <78508659+benjaminhooks@users.noreply.github.com>
2021-05-03 17:54:27 -0400

    Update Coding_Challenge_8.py

--- Assignments/Coding_Challenge8/Coding_Challenge_8.py
+++ Assignments/Coding_Challenge8/Coding_Challenge_8.py
@@ -1,17 +1,48 @@
-#Type a sentence, and the function will count the amount of times each word appears in the sentence
+# Code from challenge 5, but with a function that is called upon twice to describe shapefile parameters
+import arcpy
+from arcpy import env
+arcpy.env.overwriteOutput = True
 
-words = input("Type a sentence: ")
+env.workspace = r"C:\NRS_528\Assignment8"
 
-def word_count(x):
-    counts = dict()
-    words = x.split()
-
-    for word in words:
-        if word in counts:
-            counts[word] += 1
+def describe_shp(inFeatures):
+    desc = arcpy.Describe(inFeatures)
+    print("Describing: " + str(inFeatures))
+    if arcpy.Exists(inFeatures):
+        if desc.dataType == "ShapeFile":
+            print("Feature Type:  " + desc.shapeType)
+            print("Coordinate System Type:  " + desc.spatialReference.type)
+            print("Coordinate System used:  " + desc.spatialReference.GCSName)
         else:
-            counts[word] = 1
+            print("Input data not ShapeFile..")
+    else:
+        print("Dataset not found, please check the file path..")
 
-    return counts
 
-print(word_count(words))
+# Input point features to go through thiessen polygon execution.
+# In this case, I used emergency medical locations downloaded from RIGIS to visualize areas with the closest EMS location.
+
+
+inFeatures = "Emergency_Medical_Services.shp"
+outFeatureClass = "EMS_Thiessen"
+outFields = "ALL"
+
+describe_shp((inFeatures))
+print('Creating Thiessen Polygons from:' + inFeatures)
+
+arcpy.CreateThiessenPolygons_analysis(inFeatures, outFeatureClass, outFields)
+
+# Clip data for desired analysis location. In this example, I clipped the thiessen data to RI, as the
+# thiessen data from earlier goes beyond the extent of RI.
+
+inFeatures = "EMS_Thiessen.shp"
+clip_features = "Municipalities__1997_.shp"
+out_feature_class = "EMS_Thiessen_riclipped"
+
+describe_shp((inFeatures))
+print('Clipping study area from:' + clip_features)
+
+arcpy.Clip_analysis(inFeatures, clip_features, out_feature_class)
+
+if arcpy.Exists(out_feature_class + ".shp"):
+    print('Complete!')
