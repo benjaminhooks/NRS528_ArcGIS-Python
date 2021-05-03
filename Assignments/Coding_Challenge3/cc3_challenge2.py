86d76e8c9862448b972b9d26b1b0987e2f3e4625
Ben <78508659+benjaminhooks@users.noreply.github.com>
2021-05-03 16:54:55 -0400

    Add files via upload

---
+++ Assignments/Coding_Challenge3/cc3_challenge2.py
@@ -0,0 +1,15 @@
+import sys
+
+print("Ben Hooks - Profile")
+
+def main(arg):
+     print("Favorite Show: " + str(arg))
+main(sys.argv[1])
+
+def main(arg):
+    print("Favorite Food: " + str(arg))
+main(sys.argv[2])
+
+def main(arg):
+    print("Favorite Movie: " + str(arg))
+main(sys.argv[3])
