2a76ddde97fea6a8313c7de95b06d3a6e497b5cf
Ben <78508659+benjaminhooks@users.noreply.github.com>
2021-05-03 18:35:50 -0400

    Update Coding_Challenge_8.py

--- Assignments/Coding_Challenge8/Coding_Challenge_8.py
+++ Assignments/Coding_Challenge8/Coding_Challenge_8.py
@@ -1,9 +1,11 @@
-# Code from challenge 4, but with a function that is called upon twice to describe shapefile parameters
+# Code from challenge 5, but with a function that is called upon to describe shapefile parameters and perform analysis
 import arcpy
 from arcpy import env
 arcpy.env.overwriteOutput = True
 
 env.workspace = r"C:\NRS_528\Assignment8"
+
+# Function to describe shapefile
 
 def describe_shp(inFeatures):
     desc = arcpy.Describe(inFeatures)
@@ -18,31 +20,48 @@ def describe_shp(inFeatures):
     else:
         print("Dataset not found, please check the file path..")
 
+# Function to create thiessen polygons
 
-# Input point features to go through thiessen polygon execution.
-# In this case, I used emergency medical locations downloaded from RIGIS to visualize areas with the closest EMS location.
+def thiessen_poly(inFeatures):
+    desc = arcpy.Describe(inFeatures)
+    print("Creating thiessen polygons from : " + str(inFeatures))
+    if arcpy.Exists(inFeatures):
+        if desc.dataType == "ShapeFile":
 
+            inFeatures = "Emergency_Medical_Services.shp"
+            outFeatureClass = "EMS_Thiessen"
+            outFields = "ALL"
 
-inFeatures = "Emergency_Medical_Services.shp"
-outFeatureClass = "EMS_Thiessen"
-outFields = "ALL"
+            arcpy.CreateThiessenPolygons_analysis(inFeatures, outFeatureClass, outFields)
+        else:
+            print("Input data not ShapeFile..")
+    else:
+        print("Dataset not found, please check the file path..")
 
-describe_shp((inFeatures))
-print('Creating Thiessen Polygons from:' + inFeatures)
+# Function to clip to RI study area
 
-arcpy.CreateThiessenPolygons_analysis(inFeatures, outFeatureClass, outFields)
+def clip_shapefile(inFeatures):
+    desc = arcpy.Describe(inFeatures)
+    print("Clipping shapefile")
+    if arcpy.Exists(inFeatures):
+        if desc.dataType == "ShapeFile":
 
-# Clip data for desired analysis location. In this example, I clipped the thiessen data to RI, as the
-# thiessen data from earlier goes beyond the extent of RI.
+            inFeatures = "EMS_Thiessen.shp"
+            clip_features = "Municipalities__1997_.shp"
+            out_feature_class = "EMS_Thiessen_riclipped"
 
-inFeatures = "EMS_Thiessen.shp"
-clip_features = "Municipalities__1997_.shp"
-out_feature_class = "EMS_Thiessen_riclipped"
+            arcpy.Clip_analysis(inFeatures, clip_features, out_feature_class)
+        else:
+            print("Input data not ShapeFile..")
+    else:
+        print("Dataset not found, please check the file path..")
 
-describe_shp((inFeatures))
-print('Clipping study area from:' + clip_features)
+# Calling upon functions
 
-arcpy.Clip_analysis(inFeatures, clip_features, out_feature_class)
+describe_shp(env.workspace + "/Municipalities__1997_.shp")
+describe_shp(env.workspace + "/Emergency_Medical_Services.shp")
+thiessen_poly(env.workspace + "/Emergency_Medical_Services.shp")
+clip_shapefile(env.workspace + "/EMS_Thiessen.shp")
 
-if arcpy.Exists(out_feature_class + ".shp"):
+if arcpy.Exists("EMS_Thiessen.shp"):
     print('Complete!')
