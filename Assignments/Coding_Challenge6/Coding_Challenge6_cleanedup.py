9ada9c664db59d1677a073f56d10e05c4b797866
Ben <78508659+benjaminhooks@users.noreply.github.com>
2021-05-03 17:21:16 -0400

    Update Coding_Challenge6_cleanedup.py

--- Assignments/Coding_Challenge6/Coding_Challenge6_cleanedup.py
+++ Assignments/Coding_Challenge6/Coding_Challenge6_cleanedup.py
@@ -1,25 +1,20 @@
 import arcpy, os
 
-
 listMonths = ["02", "04", "05", "07", "10", "11"]
-outputDirectory = r"C:\Data\Students_2021\1_Data"
+outputDirectory = r"C:\NRS_528\Classes\06_Cheating\Step_3_Data"
 if not os.path.exists(outputDirectory):
     os.mkdir(outputDirectory)
-    
-from arcpy.sa import * 
-    
+
+from arcpy.sa import *
+
 for month in listMonths:
     arcpy.env.workspace = os.path.join(outputDirectory, "2015" + str(month))
     listRasters = arcpy.ListRasters("*", "TIF")
 
-    listRasterRed = [x for x in listRasters if "B4" in x]
+    listRasterVis = [x for x in listRasters if "B4" in x]
     listRasterNIR = [x for x in listRasters if "B5" in x]
 
+    print("Creating NVDI raster from " + str(listRasterVis) + str(listRasterNIR))
 
-
-    print(listRasterRed)
-    print(listRasterNIR)
-    
-    
-    output_raster = (Raster(listRasterNIR) - Raster(listRasterRed)) / (Raster(listRasterNIR) + Raster(listRasterRed))
+    output_raster = (Raster(listRasterNIR) - Raster(listRasterVis)) / (Raster(listRasterNIR) + Raster(listRasterVis))
     output_raster.save(os.path.join(outputDirectory, "2015" + str(month) + "_NDVI.tif"))
