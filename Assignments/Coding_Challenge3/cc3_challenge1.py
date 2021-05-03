cd706a85f6dc3dd7e4ca293d5db0dac86b4b8ec2
Ben <78508659+benjaminhooks@users.noreply.github.com>
2021-05-03 16:51:12 -0400

    Create cc3_challenge1.py

---
+++ Assignments/Coding_Challenge3/cc3_challenge1.py
@@ -0,0 +1,24 @@
+import os
+import shutil
+
+os.mkdir(r"C:\CC3")
+
+os.mkdir(r"C:\CC3\draft_code")
+
+os.mkdir(r"C:\CC3\draft_code\pending")
+
+os.mkdir(r"C:\CC3\draft_code\complete")
+
+os.mkdir(r"C:\CC3\includes")
+
+os.mkdir(r"C:\CC3\layouts")
+
+os.mkdir(r"C:\CC3\layouts\default")
+
+os.mkdir(r"C:\CC3\layouts\post")
+
+os.mkdir(r"C:\CC3\layouts\post\posted")
+
+os.mkdir(r"C:\CC3\site")
+
+shutil.rmtree(r"C:\CC3")
